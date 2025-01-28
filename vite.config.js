348758ac70739341de5ad6458d2630b35175b214
rodgers <rodgerstarrodgers@gmail.com>
2025-01-29 02:37:08 +0300

    ad 3

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: "/Lyma",
+  plugins: [react(), tailwindcss()],
+  base: "/Lyma/", // 
 });
