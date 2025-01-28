c0ae8e22e1436b90f1021c8a5fff1c71bf284418
rodgers <rodgerstarrodgers@gmail.com>
2025-01-29 01:00:17 +0300

    animation in the about testimonial and contact section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import {assets} from "../assets/assets.js";
+import {motion} from "motion/react";
 
 const About = () => {
     return (
-        <div className='flex flex-col items-center justify-center
+        <motion.div
+             initial={{opacity:0, x:200}}
+             transition={{duration:1}}
+             whileInView={{opacity:1, x:0}}
+             viewport={{once:true}}
+
+
+            className='flex flex-col items-center justify-center
         container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden'
              id='About'>
             <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span
@@ -44,7 +52,7 @@ const About = () => {
                     button>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 export default About
