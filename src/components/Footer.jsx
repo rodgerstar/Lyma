d8121bbfa6451b72d165668ccdddced14952ce60
rodgers <rodgerstarrodgers@gmail.com>
2025-01-29 00:29:18 +0300

    footer complete and animating the header section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,12 +10,14 @@ const Footer = () => {
             justify-between items-start'>
                 <div className='w-full md:w-1/3 mb-8 md:mb-0'>
                     <img src={assets.logo_dark} alt=""/>
-                    <p className='text-gray-400 mt-4'>Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem corporis cum dignissimos eaque, eos, esse et facilis incidunt laborum mollitia, officiis pariatur porro praesentium quam repellendus sit tempora temporibus!</p>
+                    <p className='text-gray-400 mt-4'>Lyma has a skilled team of engineers and technicians ready to handle
+                        projects across East Africa. Supported by local and international suppliers, we maintain a wide stock
+                        of spare parts for air compressors and equipment related to steam and compressed air generation.</p>
                 </div>
-                <div>
+                <div className='w-full md:w-1/5 mb-8 md:mb-0'>
                     <h3 className='text-white text-lg font-bold
                     mb-4'>Company</h3>
-                    <ul>
+                    <ul className='flex flex-col gap-2 text-gray-400'>
                         <a href="#Header"
                            className='hover:text-white'>Home</a>
                         <a href="#About"
@@ -26,9 +28,24 @@ const Footer = () => {
                            className='hover:text-white'>Privacy Policy</a>
                     </ul>
                 </div>
-                <div></div>
+                <div className='w-full md:w-1/3'>
+                    <h3 className='text-white text-lg font-bold
+                    mb-4'>Subscribe to our newsletter</h3>
+                    <p className='text-gray-400 mb-4 max-w-80'>The latest news, updates and resources, sent to your inbox
+                    weekly</p>
+                    <div className='flex gap-2'>
+                        <input type="email" placeholder='Enter your Email'
+                        className='p-2 rounded bg-gray-800 text-gray-400 broder
+                        border-gray-700 focus:outline-none w-full md:w-auto'/>
+                        <button className='py-2 px-4 rounded bg-blue-500
+                        text-white'>Subscribe</button>
+                    </div>
+                </div>
             </div>
-            <div></div>
+            <div className='border-t border-gray-700 py-4 mt-10 text-center
+            text-gray-500'>
+            Copyight 2025 @ LYMA ENGINEERING SERVICES. | Powered By Nexel.
+            </div>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
+import { motion } from "motion/react"
 
 const Header = () => {
     return (
         <div className='min-h-screen mb-4 bg-cover bg-center flex
         items-center w-full overflow-hidden' style={{backgroundImage:"url('/header_img.png')"}} id='Header'>
          <Navbar/>
-         <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32
+         <motion.div
+             initial={{opacity:0, y:100}}
+             transition={{duration:1.5}}
+             whileInView={{opacity:1, y:0}}
+             viewport={{once:true}}
+             className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32
             text-white'>
                 <h2 className='text-5xl sm:6xl md:text-[65px] inline-block
                 max-w-3xl font-semibold pt-20'>Comprehensive Engineering Services for Industrial Excellence</h2>
@@ -16,7 +22,7 @@ const Header = () => {
                     <a href="#Contact" className='bg-blue-500 border-white px-8
                     py-3 roundend'>Contact Us</a>
                 </div>
-         </div>
+         </motion.div>
         </div>
     )
 }
